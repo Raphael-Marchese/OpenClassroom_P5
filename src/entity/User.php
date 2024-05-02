9f220b644b639c80339e6b3739a5b11746e8c88d
Raphael Marchese <marchese.raphael@gmail.com>
2024-05-02 18:12:58 +0200

    Make entity, improve naming, improve templating

---
+++ src/entity/User.php
@@ -0,0 +1,80 @@
+<?php
+declare(strict_types=1);
+
+namespace App\entity;
+
+class User
+{
+    private int $id;
+    private string $firstName;
+    private string $lastName;
+    private string $username;
+    private string $email;
+    private string $password;
+    private string $role;
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): void
+    {
+        $this->role = $role;
+    }
+}
