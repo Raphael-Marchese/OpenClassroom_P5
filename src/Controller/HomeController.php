d1db725b3546ca81b4097ee82366779143e40eb0
Raphael Marchese <marchese.raphael@gmail.com>
2024-09-13 15:32:03 +0200

    Fill homepage and fix blogpost bug

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,38 @@
 
 namespace App\Controller;
 
+use App\Exception\DatabaseException;
+use App\Model\Repository\PostRepository;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
 
 class HomeController extends Controller
 {
+    private PostRepository $postRepository;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->postRepository = new PostRepository();
+    }
+
     /**
      * @throws SyntaxError
      * @throws RuntimeError
      * @throws LoaderError
+     * @throws DatabaseException
      */
     public function render(): void
     {
-        echo $this->twig->render('homepage/homepage.html.twig');
+        try {
+            $posts = $this->postRepository->findThreeLastPosts();
+
+            echo $this->twig->render('homepage/homepage.html.twig', ['posts' => $posts]);
+        } catch (DatabaseException $e) {
+            $error = $e->getMessage();
+            echo $this->twig->render('homepage/homepage.html.twig', ['error' => $error]);
+        }
     }
+
 }
